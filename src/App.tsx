f50416d6b4b7ac8305063e6446958f3d6314b1e9
Jakub Abram <j.abram1@wp.pl>
2025-11-28 10:16:31 +0100

    Finished image submission form

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { FormProvider, useForm } from "react-hook-form"
 import ImageInput from "./components/ImageInput"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { photoUploadSchema } from "./schemas/photoUploadSchema"
-import {z} from "zod"
+import { z } from "zod"
 import Button from "./components/Button"
+import { axiosPublic } from "./api/axiosPublic"
 
 type PhotoFormFields = z.infer<typeof photoUploadSchema>
 
@@ -13,25 +14,53 @@ function App() {
     resolver: zodResolver(photoUploadSchema)
   })
 
-  const {formState: {errors}, handleSubmit} = methods
+  const { formState: { errors }, handleSubmit } = methods
 
   const onSubmit = (data: PhotoFormFields) => {
+    console.log("submitting")
+    const handleImageSubmission = async (chunksArray: string[], chunksAmount: number, size: Number) => {
+      try {
+        const declarationResponse = await axiosPublic.post(
+          "/image/declaration",
+          { size, chunksAmount },
+          { headers: { "Content-Type": "application/json" } }
+        )
 
-    const handleImageSubmission = async (chunksArray: string[], chunksSize: number) => {
+        const { declaration_id: image_id } = declarationResponse.data
+        const promisesArray = []
+        for(let i = 0; i < chunksAmount; i++){
+          promisesArray.push(i+1)
+        }
+
+        const responses = await Promise.all(promisesArray.map((chunkNumber) => {
+          return axiosPublic.post(
+            "/image/upload",
+            {  image_id, chunksNumber: chunkNumber, data: chunksArray[chunkNumber-1]},
+            { headers: {"Content-Type": "application/json"}}
+          )
+        }))
+        console.log(responses)
+      } catch (err) {
+        console.log(err)
+      }
     }
 
     const fileReader = new FileReader()
-    if(data.photo instanceof FileList){
+    if (data.photo instanceof FileList) {
+      const size = data.photo[0].size
+      const chunkSize = 100000
       fileReader.readAsDataURL(data.photo[0])
-      fileReader.addEventListener("load", () => {
-        const {result} = fileReader
-        if(typeof result === "string"){
+      fileReader.addEventListener("load", async () => {
+        const { result } = fileReader
+        if (typeof result === "string") {
           const length = result.length
-          const chunkSize = Math.ceil(length / 200000)
+          const chunksAmount = Math.ceil(length / chunkSize)
           const chunksArray: string[] = []
-          for(let i = 0; i < chunkSize; i++){
+          for (let i = 0; i < chunksAmount; i++) {
             chunksArray.push(result.slice(i * chunkSize, (i + 1) * chunkSize))
           }
+          console.log(chunksArray)
+          await handleImageSubmission(chunksArray, chunksAmount, size)
         }
       })
     }
