362c3d396d9f867cfff32c9ea92d9a5039b8f498
Jakub Abram <j.abram1@wp.pl>
2025-11-28 10:32:31 +0100

    Created pages and their routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <></>
+import { Routes, Route, Navigate } from "react-router"
+import Nav from "./layouts/Nav"
+import HomePage from "./pages/HomePage"
+import ImageUplaodPage from "./pages/ImageUploadPage"
+
+export const BASE_PAGE = "/images"
+
+export default function App() {
+  return <Routes>
+    <Route path={BASE_PAGE} element={<Nav />}>
+      <Route index element={<HomePage />}/>
+      <Route path="upload-photo" element={<ImageUplaodPage />}/>
+    </Route>
+
+    <Route path="/*" element={<Navigate to={BASE_PAGE} />} />
+  </Routes>
 }
 
   
---
+++ src/pages/ImageUploadPage.tsx
@@ -0,0 +1,5 @@
+export default function ImageUplaodPage(){
+    return <h1>
+        Image Upload Page
+    </h1>
+}
