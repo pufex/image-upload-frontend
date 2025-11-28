ad42f6a437ddcdc71f43d254646e4f914317297d
Jakub Abram <j.abram1@wp.pl>
2025-11-28 19:46:11 +0100

    Created routing for auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,34 @@ import Nav from "./layouts/Nav"
 import HomePage from "./pages/HomePage"
 import ImageUplaodPage from "./pages/ImageUploadPage"
 import SinglePhotoPage from "./pages/SinglePhotoPage"
+import UnloggedOnlyRoute from "./auth/components/UnloggedOnlyRoute"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
 
 export const BASE_PAGE = "/photos"
 
 export default function App() {
   return <Routes>
-    <Route path={BASE_PAGE} element={<Nav />}>
-      <Route index element={<HomePage />}/>
-      <Route path=":id" element={<SinglePhotoPage />}/>
-      <Route path="upload-photo" element={<ImageUplaodPage />}/>
+    <Route element={<Nav />}>
+
+      <Route path={BASE_PAGE} >
+        <Route index element={<HomePage />} />
+        <Route path=":id" element={<SinglePhotoPage />} />
+        <Route path="upload-photo" element={<ImageUplaodPage />} />
+      </Route>
+
+      <Route path="/auth" element={<UnloggedOnlyRoute />}>
+        <Route path="login" element={<LoginPage />}/>
+        <Route path="register" element={<RegisterPage />}/>
+      </Route>
+
     </Route>
+
 
     <Route path="/*" element={<Navigate to={BASE_PAGE} />} />
   </Routes>
 }
 
-  
 
-  
+
+
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "../auth/forms/RegisterForm"
+
+export default function RegisterPage (){
+    return <>
+        <h1 className="text-2xl text-center text-black font-bold mb-8 pt-8">
+            Register Page
+        </h1>
+        <RegisterForm />
+    </>
+}
